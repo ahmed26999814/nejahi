e00ebc4df2b4d5baae59d660789adf2628a61047
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-10 19:08:43 +0000

    Mount result detail sanitizer

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
 import HomeApplication from "../components/home/HomeApplication";
+import ResultDetailSanitizer from "../components/results/ResultDetailSanitizer";
 
 export default function HomePage() {
-  return <HomeApplication />;
+  return (
+    <>
+      <HomeApplication />
+      <ResultDetailSanitizer />
+    </>
+  );
 }
