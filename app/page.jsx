5cecc79118df28ae9b363c459733678f0ceec3db
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-12 18:33:06 +0000

    Place ads below the main experience

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import HomeApplication from "../components/home/HomeApplication";
 import ResultDetailSanitizer from "../components/results/ResultDetailSanitizer";
+import AdsterraAds from "../components/ads/AdsterraAds";
 
 export default function HomePage() {
   return (
     <>
       <HomeApplication />
+      <AdsterraAds />
       <ResultDetailSanitizer />
     </>
   );
