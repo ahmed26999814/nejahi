1dad7515490dc7e1b74362f2de031b47b7959e03
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-12 18:37:20 +0000

    Remove Adsterra ads from homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,10 @@
 import HomeApplication from "../components/home/HomeApplication";
 import ResultDetailSanitizer from "../components/results/ResultDetailSanitizer";
-import AdsterraAds from "../components/ads/AdsterraAds";
 
 export default function HomePage() {
   return (
     <>
       <HomeApplication />
-      <AdsterraAds />
       <ResultDetailSanitizer />
     </>
   );
