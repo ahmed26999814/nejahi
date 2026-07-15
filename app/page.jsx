d705a611bc5456c9f4cbd05acdc3c36d1fa2ccbc
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-15 05:03:36 -1100

    Show results countdown on homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import HomeApplication from "../components/home/HomeApplication";
+import ResultsCountdownBanner from "../components/home/ResultsCountdownBanner";
 import ResultDetailSanitizer from "../components/results/ResultDetailSanitizer";
 
 export default function HomePage() {
   return (
     <>
+      <ResultsCountdownBanner />
       <HomeApplication />
       <ResultDetailSanitizer />
     </>
