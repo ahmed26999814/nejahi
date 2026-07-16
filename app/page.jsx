306a96c69f03dbea8a934bce3db0747804961f81
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-16 04:14:55 -1100

    إزالة إعلان النتائج من أعلى الصفحة

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,9 @@
 import HomeApplication from "../components/home/HomeApplication";
-import ResultsCountdownBanner from "../components/home/ResultsCountdownBanner";
 import ResultDetailSanitizer from "../components/results/ResultDetailSanitizer";
 
 export default function HomePage() {
   return (
     <>
-      <ResultsCountdownBanner />
       <HomeApplication />
       <ResultDetailSanitizer />
     </>
