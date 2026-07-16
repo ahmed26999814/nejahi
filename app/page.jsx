144d404165573b3d6555fc320e9e4ba145e718f8
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-16 09:35:35 -1100

    Apply compact BEPC subject details layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import HomeApplication from "../components/home/HomeApplication";
 import ResultDetailSanitizer from "../components/results/ResultDetailSanitizer";
+import BepcSubjectDetailsCompactEnhancer from "../components/results/BepcSubjectDetailsCompactEnhancer";
 
 export default function HomePage() {
   return (
     <>
       <HomeApplication />
       <ResultDetailSanitizer />
+      <BepcSubjectDetailsCompactEnhancer />
     </>
   );
 }
