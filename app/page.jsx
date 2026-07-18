c7c83eb451f873a0ca42c8eee20e5041228e120f
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-18 09:15:32 -1100

    Restore mobile search helpers

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
+import "./forgot-number.css";
+import "./keyboard-mobile.css";
 import HomeApplication from "../components/home/HomeApplication";
+import KeyboardViewportGuard from "../components/layout/KeyboardViewportGuard";
 import ResultDetailSanitizer from "../components/results/ResultDetailSanitizer";
 
 export default function HomePage() {
   return (
     <>
+      <KeyboardViewportGuard />
       <HomeApplication />
       <ResultDetailSanitizer />
     </>
