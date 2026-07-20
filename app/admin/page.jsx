705f7de3a6537a0053261f0c410860c41afe52f3
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-20 03:16:24 -1100

    Add clear link to Bac alert registrations in admin

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,10 +1,19 @@
+import Link from "next/link";
 import AdminSecretStorageBridge from "../../components/admin/AdminSecretStorageBridge";
 import ContentAdminApplication from "../../components/admin/ContentAdminApplication";
 
 export default function AdminPage() {
   return (
     <AdminSecretStorageBridge>
-      <ContentAdminApplication />
+      <>
+        <Link
+          href="/admin/alerts"
+          className="fixed left-3 top-3 z-[70] inline-flex min-h-11 items-center justify-center rounded-2xl bg-mauri-green px-4 text-xs font-black text-white shadow-[0_12px_32px_rgba(21,128,61,.28)] transition active:scale-[.97]"
+        >
+          طلبات إشعار باكالوريا
+        </Link>
+        <ContentAdminApplication />
+      </>
     </AdminSecretStorageBridge>
   );
 }
