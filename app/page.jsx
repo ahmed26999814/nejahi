4c4e9c9d56691b0c0cb5b28ea0dd93b77bed3f08
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-20 17:20:42 -1100

    Load non-critical home scripts after the main application

--- app/page.jsx
+++ app/page.jsx
@@ -1,27 +1,17 @@
 import "./forgot-number.css";
 import "./keyboard-mobile.css";
 import HomeApplication from "../components/home/HomeApplication";
-import Bac2026CountdownNotice from "../components/layout/Bac2026CountdownNotice";
+import HomeDeferredEnhancements from "../components/home/HomeDeferredEnhancements";
 import KeyboardViewportGuard from "../components/layout/KeyboardViewportGuard";
 import PublicDataFetchBridge from "../components/layout/PublicDataFetchBridge";
-import OrientationResultBridge from "../components/orientation/OrientationResultBridge";
-import ResultDetailSanitizer from "../components/results/ResultDetailSanitizer";
-import ResultSubjectDetailsBridge from "../components/results/ResultSubjectDetailsBridge";
-import BacTopperTrackOrder from "../components/ui/BacTopperTrackOrder";
-import MotivationalVisibility from "../components/ui/MotivationalVisibility";
 
 export default function HomePage() {
   return (
     <>
-      <Bac2026CountdownNotice />
       <PublicDataFetchBridge />
       <KeyboardViewportGuard />
-      <MotivationalVisibility />
-      <BacTopperTrackOrder />
-      <ResultSubjectDetailsBridge />
       <HomeApplication />
-      <ResultDetailSanitizer />
-      <OrientationResultBridge />
+      <HomeDeferredEnhancements />
     </>
   );
 }
