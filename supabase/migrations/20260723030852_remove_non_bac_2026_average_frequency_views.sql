73c8ecb9296786dd86c419ded403793f67d289e5
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-22 16:09:14 -1100

    Remove average frequency views for other exams

---
+++ supabase/migrations/20260723030852_remove_non_bac_2026_average_frequency_views.sql
@@ -0,0 +1,4 @@
+drop view if exists public.bac_average_frequency;
+drop view if exists public.bac_session2_average_frequency;
+drop view if exists public.brevet_average_frequency;
+drop view if exists public.excellence_1as_average_frequency;
