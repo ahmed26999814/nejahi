2b5e5d588a47cbb3d7f6d56153eb3ac36d71a474
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-22 16:02:47 -1100

    Show average frequency only for Bac 2026 analytics

---
+++ app/page.js
@@ -0,0 +1,21 @@
+import "./forgot-number.css";
+import "./keyboard-mobile.css";
+import HomeApplication from "../components/home/HomeApplication";
+import HomeDeferredEnhancements from "../components/home/HomeDeferredEnhancements";
+import Bac2026AverageFrequency from "../components/analytics/Bac2026AverageFrequency";
+import Bac2026CountdownNotice from "../components/layout/Bac2026CountdownNotice";
+import KeyboardViewportGuard from "../components/layout/KeyboardViewportGuard";
+import PublicDataFetchBridge from "../components/layout/PublicDataFetchBridge";
+
+export default function HomePage() {
+  return (
+    <>
+      <PublicDataFetchBridge />
+      <KeyboardViewportGuard />
+      <Bac2026CountdownNotice />
+      <HomeApplication />
+      <Bac2026AverageFrequency />
+      <HomeDeferredEnhancements />
+    </>
+  );
+}
