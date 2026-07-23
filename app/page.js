f2b54c8255e8afa772e7ec3d59583d71fad6bccd
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-07-22 16:34:34 -1100

    Remove average frequency from analytics shell

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ import "./forgot-number.css";
 import "./keyboard-mobile.css";
 import HomeApplication from "../components/home/HomeApplication";
 import HomeDeferredEnhancements from "../components/home/HomeDeferredEnhancements";
-import Bac2026AverageFrequency from "../components/analytics/Bac2026AverageFrequency";
 import Bac2026CountdownNotice from "../components/layout/Bac2026CountdownNotice";
 import KeyboardViewportGuard from "../components/layout/KeyboardViewportGuard";
 import PublicDataFetchBridge from "../components/layout/PublicDataFetchBridge";
@@ -14,7 +13,6 @@ export default function HomePage() {
       <KeyboardViewportGuard />
       <Bac2026CountdownNotice />
       <HomeApplication />
-      <Bac2026AverageFrequency />
       <HomeDeferredEnhancements />
     </>
   );
