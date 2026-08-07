9055b620c99712e6762ee933888c256ff0528253
ahmed26999814 <ahmedabdellahi2699@gmail.com>
2026-08-07 18:39:53 +0000

    Rename admin result alerts link

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -10,7 +10,7 @@ export default function AdminPage() {
           href="/admin/alerts"
           className="fixed left-3 top-3 z-[70] inline-flex min-h-11 items-center justify-center rounded-2xl bg-mauri-green px-4 text-xs font-black text-white shadow-[0_12px_32px_rgba(21,128,61,.28)] transition active:scale-[.97]"
         >
-          طلبات إشعار باكالوريا
+          طلبات إشعارات النتائج
         </Link>
         <ContentAdminApplication />
       </>
